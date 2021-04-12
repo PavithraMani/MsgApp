f7505601d9d8ad477d960cb258d94cc7666d100a
pavimanipnm <79173319+pavimanipnm@users.noreply.github.com>
2021-04-12 12:09:12 -0700

    chatroom app with socket

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "testu1", password: "heyhey")
+User.create(username: "testu2", password: "heyhey")
